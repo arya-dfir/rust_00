4a05aa989c3abe9e7efb0966bfa6aba419d74503
arya-dfir <contact.aryatripathi@gmail.com>
2025-11-04 19:04:52 +0100

    Initial Rust project setup

---
+++ rust_00/rust_00/target/debug/deps/rust_00.d
@@ -0,0 +1,5 @@
+C:\Users\Shadow\rust_bootcamp\rust_00\rust_00\target\debug\deps\rust_00.d: src\main.rs
+
+C:\Users\Shadow\rust_bootcamp\rust_00\rust_00\target\debug\deps\rust_00.exe: src\main.rs
+
+src\main.rs:
---
+++ rust_00/rust_00/target/debug/rust_00.d
@@ -0,0 +1,1 @@
+C:\Users\Shadow\rust_bootcamp\rust_00\rust_00\target\debug\rust_00.exe: C:\Users\Shadow\rust_bootcamp\rust_00\rust_00\src\main.rs
